dc5141f9e94440a6281070db654acdfdc8e7344d
Fernando <fernandodarolt@hotmail.com>
2022-11-17 21:39:00 -0300

    Exibir senha, event, propety e two way data biding

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,14 +14,14 @@ export class HomePage {
   ) {
     this.carregar();
   }
+  
   carregar(){
-    //this.site   = this.local_storage.get('site');
-    //this.login  = this.local_storage.get('login');
-    //this.senha  = this.local_storage.get('senha');
   }
+
   armazenar(){
     this.local_storage.append('credenciais',this._credencial);
   }
+  
   limpar(){
     // Serviço que limpa os dados do local storage
     this.local_storage.clear();
--- src/app/listar/listar.page.ts
+++ src/app/listar/listar.page.ts
@@ -9,12 +9,26 @@ import { LocalStorageService } from '../local-storage.service';
 })
 export class ListarPage implements OnInit {
   public data:Array<Credencial> = [];
+  public icon_type:string = 'eye-outline';
+  public pass_type:string = 'password' ;
+  
   constructor(
     public local_storage:LocalStorageService
   ) {}
 
   ngOnInit() {
     this.carregar();
+  }
+
+  exibir(){
+    if (this.icon_type == 'eye-outline'){
+        this.icon_type = 'eye-off-outline';
+        this.pass_type = 'text';
+    } else {
+      this.icon_type = 'eye-outline';
+      this.pass_type = 'password';
+    }
+
   }
 
   carregar(){
